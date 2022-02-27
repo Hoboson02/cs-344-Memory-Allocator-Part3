cc3426a80896b21b04dbeca410dbb5d3719de68b
Ryan Earl <ryanleoearl@gmail.com>
2022-02-26 22:16:38 -0800

    Completion of coalescing loop in myfree

--- myalloc.c
+++ myalloc.c
@@ -43,15 +43,7 @@ void *myalloc(size_t size) {
     while (cur != NULL) {
         // if this node is big enough and not in use:
         if (!cur->in_use && cur->size >= padded_size) {
-            split_space(cur, size);
-	    // start cur at head
-
-		// while cur->next isn't NULL:
-		// 	if cur is not in_use and next node not in use:
-		// 		add the next node's region's size to cur's
-		// 		make cur's next pointer skip the next node
-		// 	else
-		// 		move cur to next node		
+            split_space(cur, size);	
             // mark it in use
             cur->in_use = 1;
             // printf("Found one!\n");
@@ -80,15 +72,29 @@ void split_space (struct block *s, size_t size) {
 void myfree (void *p) { 
     struct block *b = p - (block_padded_size);
     b->in_use = 0;
+	struct block *cur = head; // start cur at head
+	while (cur->next != NULL) { // while cur->next isn't NULL:
+		if ((cur->in_use == 0) && (cur->next->in_use == 0)) {// 	if cur is not in_use and next node not in use:
+			cur->size += block_padded_size + (cur->next)->size;	// 		add the next node's region's size to cur's
+			cur->next = cur->next->next;	// 		make cur's next pointer skip the next node
+		}
+		else { // 	else
+			cur = cur->next;	// 		move cur to next node
+		}		
+		
+	}
 }
 
 int main(void) {
-    void *p;
+    void *p, *q, *r, *s;
 
-    myalloc(10);     print_data();
-    p = myalloc(20); print_data();
-    myalloc(30);     print_data();
-    myfree(p);       print_data();
-    myalloc(40);     print_data();
-    myalloc(10);     print_data();
+p = myalloc(10); print_data();
+q = myalloc(20); print_data();
+r = myalloc(30); print_data();
+s = myalloc(40); print_data();
+
+myfree(q); print_data();
+myfree(p); print_data();
+myfree(s); print_data();
+myfree(r); print_data();
 }
